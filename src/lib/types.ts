1fcc4ff6a0ba8796408f023a2c97805837e5a041
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-09-11 12:48:03 -0700

    Added image for README

---
+++ src/lib/types.ts
@@ -0,0 +1,16 @@
+export type CreateLinkModalProps = {
+  projectId: string;
+};
+
+export type DeleteLinkModalProps = {
+  linkId: string;
+};
+
+export type DeleteProjectModalProps = {
+  projectId: string;
+};
+
+export type UpdateProjectModalProps = {
+  projectId: string;
+  projectName: string;
+};
