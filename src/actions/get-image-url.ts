fec5dbed5a761040e891cd7b65de92811be064eb
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-09-12 12:53:54 -0700

    Updated getScreenshotUrl

--- src/actions/get-image-url.ts
+++ src/actions/get-image-url.ts
@@ -7,50 +7,71 @@ export const getScreenshotUrl = async (linkUrl: string, type: string) => {
   const screenlyApiEndpoint = process.env.NEXT_PUBLIC_SCREENLY_API_ENDPOINT;
   const mobileWidth = 390;
   const desktopWidth = 1440;
-  let windowWidth;
 
-  switch (type) {
-    case 'desktop':
-      windowWidth = desktopWidth;
-      break;
-    case 'mobile':
-      windowWidth = mobileWidth;
-      break;
-    default:
-      windowWidth = desktopWidth;
-      break;
-  }
-
-  try {
-    if (screenlyApiEndpoint) {
-      const response = await axios.post(
-        screenlyApiEndpoint,
-        {
-          url: linkUrl,
-          full_page: '1',
-          timeout: 30,
-          window_width: windowWidth,
-          css_media_type: 'screen',
-          delay: '100',
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${screenlyApiKey}`,
+  if (type == 'desktop') {
+    try {
+      if (screenlyApiEndpoint) {
+        const response = await axios.post(
+          screenlyApiEndpoint,
+          {
+            url: linkUrl,
+            full_page: '1',
+            timeout: 30,
+            window_width: desktopWidth,
+            css_media_type: 'screen',
+            delay: '100',
           },
-        },
-      );
-
-      if (response.status === 201) {
-        const urlToDownloadFrom = response.data.data.shot_url;
-        return urlToDownloadFrom;
-      } else {
-        throw new Error(
-          `Screenshot capture failed with status code: ${response.status}`,
+          {
+            headers: {
+              Authorization: `Bearer ${screenlyApiKey}`,
+            },
+          },
         );
+
+        if (response.status === 201) {
+          const urlToDownloadFrom = response.data.data.shot_url;
+          return urlToDownloadFrom;
+        } else {
+          throw new Error(
+            `Screenshot capture failed with status code: ${response.status}`,
+          );
+        }
       }
+    } catch (error) {
+      throw new Error(`Screenshot capture failed: ${error}`);
     }
-  } catch (error) {
-    throw new Error(`Screenshot capture failed: ${error}`);
+  } else if (type == 'mobile') {
+    try {
+      if (screenlyApiEndpoint) {
+        const response = await axios.post(
+          screenlyApiEndpoint,
+          {
+            url: linkUrl,
+            full_page: '1',
+            timeout: 30,
+            window_width: mobileWidth,
+            css_media_type: 'screen',
+            delay: '100',
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${screenlyApiKey}`,
+            },
+          },
+        );
+
+        if (response.status === 201) {
+          const urlToDownloadFrom = response.data.data.shot_url;
+          return urlToDownloadFrom;
+        } else {
+          throw new Error(
+            `Screenshot capture failed with status code: ${response.status}`,
+          );
+        }
+      }
+    } catch (error) {
+      throw new Error(`Screenshot capture failed: ${error}`);
+    }
   }
 };
 
