30d03a48ae56d6f283b63d91900062e4b7ec7dd6
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-10-09 14:47:23 -0700

    Added app icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,21 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning={true}>
+        <head>
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link
+            rel="icon"
+            href="/icon?<generated>"
+            type="image/<generated>"
+            sizes="<generated>"
+          />
+          <link
+            rel="apple-touch-icon"
+            href="/apple-icon?<generated>"
+            type="image/<generated>"
+            sizes="<generated>"
+          />
+        </head>
         <body>
           <TanstackProvider>
             {children}
